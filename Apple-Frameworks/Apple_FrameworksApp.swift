0ed17725777488e5bf0c2d56e8b755981872fde8
Sammy Cannillo <sammyjaynecannillo@gmail.com>
2022-11-04 13:07:45 -0500

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Samantha Cannillo on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
